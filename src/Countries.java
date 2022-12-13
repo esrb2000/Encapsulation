801daefc41993fc94edd442078b7cb7286b4ef7b
Kuptsevich Boris <esrb2000@bk.ru>
2022-12-13 16:59:31 +0700

    add countries

---
+++ src/Countries.java
@@ -0,0 +1,15 @@
+public class Countries {
+    String name;
+    double square;
+    int population;
+    boolean exitToSea;
+
+    public void add (String name, double square, int population, boolean exitToSea) {
+        this.name = name;
+        this.square = square;
+        this.population = population;
+        this.exitToSea = exitToSea;
+    }
+
+
+}
